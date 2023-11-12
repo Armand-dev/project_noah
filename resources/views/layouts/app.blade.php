6d1224715038bf6cd7b040ee4fdbae75e4d12531
Armand Dev <codarmi@gmail.com>
2023-11-12 18:42:38 +0200

    fixed socket io import

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script src="http://localhost:6003/socket.io/socket.io.js"></script>
+    <script src="{{env('APP_URL')}}/socket.io/socket.io.js"></script>
 </head>
 
 <body class="font-sans antialiased">
