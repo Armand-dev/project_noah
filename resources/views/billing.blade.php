f787e54b1c2c55d030a2f98283cc227e572aae99
Armand Dev <codarmi@gmail.com>
2023-11-30 01:28:32 +0200

    removed pricing table added billing portal

--- resources/views/billing.blade.php
+++ resources/views/billing.blade.php
@@ -8,15 +8,16 @@
     </x-slot>
 
     <div class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
-
-        <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
-        <stripe-pricing-table
-            pricing-table-id="prctbl_1OHvnkHKKgkGW03ID2ctpWMA"
-            publishable-key="pk_test_51OHvY4HKKgkGW03IGAHW50OT4mOucOpPDougtERJyQ5Eli0WT38gUVSxy5XZw8p0domg7NEin4cVs34MpSiooJwh00nMt42koJ"
-            client-reference-id="{{ auth()->user()->id }}"
-            customer-email="{{ auth()->user()->email }}"
-        >
-        </stripe-pricing-table>
-
+        Your subscription is inactive. Redirecting to billing portal in <span id="seconds">6</span> seconds...
     </div>
+
+    <script>
+        setInterval(() => {
+            document.querySelector('#seconds').innerHTML = parseInt(document.querySelector('#seconds').innerHTML) - 1;
+        }, 1000);
+
+        setTimeout(()=>{
+            window.location.href = '{{ config('billing.portal_url') }}'
+        }, 5000);
+    </script>
 </x-app-layout>
