2c3531de5e90686faee5ddd94e6e13156be573c6
Armand Dev <codarmi@gmail.com>
2023-11-30 01:47:16 +0200

    added live customer portal

--- config/billing.php
+++ config/billing.php
@@ -9,5 +9,5 @@ return [
      * Stripe Billing Portal URL
      *
      */
-    'portal_url' => 'https://billing.stripe.com/p/login/test_fZefZs3wc7Y5eAw6oo'
+    'portal_url' => 'https://billing.stripe.com/p/login/00g010gUf36t288cMM'
 ];
